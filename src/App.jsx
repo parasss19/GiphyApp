ad667922dce4d2425d03d844b0e2c5ca1e98f0a5
Paras <mehtaparas1901@gmail.com>
2025-02-21 22:07:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+//new way of writing routes (diff than we learn in codevolution)
+
+
+const App = () => {
+  return (
+    <div>
+      <h3 className='bg-amber-300'>Hello world</h3>
+    </div>
+  )
+}
+
+export default App
