54d36477e9730aa18daa738503989057b46440c8
Paras <mehtaparas1901@gmail.com>
2025-02-21 22:13:14 +0530

    pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import AppLayout from './layout/AppLayout'
+import HomePage from './pages/HomePage'
 
 
 //new way of writing routes (diff than we learn in codevolution)
+const router = createBrowserRouter([
+  {
+    //this element render on every route
+    element: <AppLayout/>,   
 
+    children: [
+      {
+       path: '/',
+       element: <HomePage/>
+      }
+      
+  ]
+
+    
+  }
+])
 
 const App = () => {
   return (
---
+++ src/pages/SingleGifPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleGifPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SingleGifPage
