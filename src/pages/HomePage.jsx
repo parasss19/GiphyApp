54d30093435c8319d098c98400b3a3d6bcddc5f3
Paras <mehtaparas1901@gmail.com>
2025-02-24 13:50:15 +0530

    gifs rendered on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { GifContext } from '../context/GifContext'
+import Gif from '../components/Gif'
 
 const HomePage = () => {
+  const {gf, gifs, setGifs, filter} = useContext(GifContext)
+
+  //rendering trending gifs on homepage
+  const fetchTrendingGifs = async () =>{
+    const {data} = await gf.trending({
+      limit: 30,
+      type: filter,
+      rating: "g",
+    })
+    console.log(data)
+    setGifs(data)
+  }
+  //we call fetchTrendingGifs func whenever filter state change and new gifs rendered on homepage
+  useEffect(()=>{
+    fetchTrendingGifs()
+  },[filter])
+
   return (
     <div>
-      Home page
+      <img src="/banner.gif" alt="banner image" className='mt-3 mb-4 rounded w-full'/>
+      
+      <div className='columns-2 md:columns-3 lg:columns-4 gap-2'>
+        { //we are mapping each gif from gifs(state of array) and passing each gif to Gif comp as a prop
+          gifs.map((gif) => (
+            <Gif gif = {gif} key={gif.title}/>
+          ))
+        }
+      </div>
+      
     </div>
   )
 }
