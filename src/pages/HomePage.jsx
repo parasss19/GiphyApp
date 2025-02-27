c84a22379668c42f73422980bea124a830bb76f5
Paras <mehtaparas1901@gmail.com>
2025-02-27 14:47:16 +0530

    gifs limits modify

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { GifContext } from '../context/GifContext'
 import Gif from '../components/Gif'
+import FilterGifs from '../components/FilterGifs'
 
 const HomePage = () => {
   const {gf, gifs, setGifs, filter} = useContext(GifContext)
@@ -8,7 +9,7 @@ const HomePage = () => {
   //rendering trending gifs on homepage
   const fetchTrendingGifs = async () =>{
     const {data} = await gf.trending({
-      limit: 30,
+      limit: 25,
       type: filter,
       rating: "g",
     })
@@ -22,16 +23,20 @@ const HomePage = () => {
 
   return (
     <div>
-      <img src="/banner.gif" alt="banner image" className='mt-3 mb-4 rounded w-full'/>
+      {/* Banner Image */}
+      <img src="/banner.gif" alt="banner image" className='mt-3 rounded w-full'/>
       
+      {/* Gifs filter */}
+      <FilterGifs showTrendingIcon ={true}/>
+      
+      {/* Gifs rendering */}
       <div className='columns-2 md:columns-3 lg:columns-4 gap-2'>
         { //we are mapping each gif from gifs(state of array) and passing each gif to Gif comp as a prop
           gifs.map((gif) => (
-            <Gif gif = {gif} key={gif.title}/>
+            <Gif gif = {gif} key={gif.id}/>
           ))
         }
       </div>
-      
     </div>
   )
 }
