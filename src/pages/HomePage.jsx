f2686d11d965693cb2bfb34ae9076ddf22febf91
Paras <mehtaparas1901@gmail.com>
2025-03-06 18:58:25 +0530

    lazy load the gifs rendering

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
-import React, { useContext, useEffect } from 'react'
+import React, { lazy, Suspense, useContext, useEffect } from 'react'
+
 import { GifContext } from '../context/GifContext'
-import Gif from '../components/Gif'
 import FilterGifs from '../components/FilterGifs'
+
+import Loading from '../components/Loader'             //our loader component
+const Gif = lazy(() => import("../components/Gif"));   //Lazy-load the Gif component
+
 
 const HomePage = () => {
   const {gf, gifs, setGifs, filter} = useContext(GifContext)
@@ -13,7 +17,7 @@ const HomePage = () => {
       type: filter,
       rating: "g",
     })
-    console.log(data)
+    //console.log(data)
     setGifs(data)
   }
   //we call fetchTrendingGifs func whenever filter state change and new gifs rendered on homepage
@@ -30,13 +34,14 @@ const HomePage = () => {
       <FilterGifs showTrendingIcon ={true}/>
       
       {/* Gifs rendering */}
-      <div className='columns-2 md:columns-3 lg:columns-4 gap-2'>
-        { //we are mapping each gif from gifs(state of array) and passing each gif to Gif comp as a prop
-          gifs.map((gif) => (
-            <Gif gif = {gif} key={gif.id}/>
-          ))
-        }
-      </div>
+      <Suspense fallback={<Loading />}>
+        <div className='columns-2 md:columns-3 lg:columns-4 gap-2'>
+          {/*we are mapping each gif from gifs(state of array) and passing each gif to Gif comp as a prop */}
+          {gifs.map((gif) => (
+            <Gif gif={gif} key={gif.id} />
+          ))}
+        </div>
+      </Suspense>
     </div>
   )
 }
