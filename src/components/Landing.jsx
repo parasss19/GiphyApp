b62ab19c85dc42032319b1876af8df3ee8f0be95
Paras <mehtaparas1901@gmail.com>
2025-10-13 15:13:27 +0530

    added peerlist badge

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -38,6 +38,16 @@ const Landing = () => {
         }}
       >
       </div>
+
+      <div>
+        <a href="https://peerlist.io/paras19/project/stickyy" target="_blank" rel="noreferrer">
+				<img
+					src="https://peerlist.io/api/v1/projects/embed/PRJHR8D6N6DAMRLE81AP6K88N6GR9L?showUpvote=false&theme=dark"
+					alt="Stickyy"
+					// style="width: auto; height: 72px;"
+				/>
+			</a>
+      </div>
       
       {/* Heading */}
       <motion.h1
